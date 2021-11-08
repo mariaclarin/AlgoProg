6390e3433040b2692592063a658f15961021b70a
mariaclarin <maria.clarin@binus.ac.id>
2021-11-08 12:19:23 +0700

    uploaded

---
+++ Exercise 3 (Files)/hapax.py
@@ -0,0 +1,37 @@
+import os 
+import string
+def hapax(all) :
+    words = all.split()
+
+    for word in words :
+        if word in count :
+            count[word] += 1
+        else : 
+            count[word] = 1
+    return count
+
+# def counter(all):
+#     data = {}
+#     for word in all.split():
+#         data[word] = data.get(word,0)+1
+#     return data
+
+os.chdir("C:/Users/clari/Documents/GitHub/AlgoProg/Exercise 3 (Files)/")
+file = open("ebook.txt", "r")
+all = file.read().lower()
+words_compilation = ""
+
+for word in all :
+    if word not in string.punctuation :
+        words_compilation += word
+all = words_compilation
+
+count = dict()
+count = hapax(all)
+
+for i in count :
+    if count[i] == 1 :
+        print(i,",", end="")
+
+file.close()
+
