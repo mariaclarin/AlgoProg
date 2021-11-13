ea7d621c2ffe39a58a9f588d1ac65fc05193d6c6
mariaclarin <maria.clarin@binus.ac.id>
2021-11-13 16:54:34 +0700

    updated

--- Exercise 3 (Files)/wordlength.py
+++ Exercise 3 (Files)/wordlength.py
@@ -1,7 +1,7 @@
-# import os 
+import os 
 import re
 
-# os.chdir("C:/Users/clari/Documents/GitHub/AlgoProg/Exercise 3 (Files)/")
+os.chdir("C:/Users/clari/Documents/GitHub/AlgoProg/Exercise 3 (Files)/")
 file = open("ebook.txt", "r")
 all = re.findall('\w+', file.read()) #finding all words in the file 
 average = sum([len(word) for word in all]) / len(all) #average formula, length of each word divided by the number of words found in the file
